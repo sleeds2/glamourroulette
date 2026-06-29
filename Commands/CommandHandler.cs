b39f243cc9808d5434cb243373d1604a5bba117a
sleeds2 <sleeds2@gmail.com>
2026-06-28 23:43:27 -0500

    Update CommandHandler.cs

--- Commands/CommandHandler.cs
+++ Commands/CommandHandler.cs
@@ -28,8 +28,8 @@ internal sealed class CommandHandler : IDisposable
         var mainCommandInfo = new CommandInfo(this.OnCommand)
         {
             HelpMessage = "Choose a random enabled glamour plate.\n"
-                        +"   /glamroulette, /gr: Alias for /glamourroulette\n" 
-                        +"   /glamourroulette config|settings - Toggle the settings window.",
+                        +"  /glamroulette|/gr - Alias for /glamourroulette\n" 
+                        +"  /glamourroulette config|settings - Toggle the settings window.",
             ShowInHelp = true,
         };
         
